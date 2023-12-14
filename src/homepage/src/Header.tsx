77da5ac4f73c33797314552b9fe31130e12f6e00
yui963 <myyy20021225@yahoo.ne.jp>
2023-12-14 13:58:05 +0900

    fix

--- src/homepage/src/Header.tsx
+++ src/homepage/src/Header.tsx
@@ -1,11 +1,6 @@
 import { AppBar, Container, Toolbar } from "@mui/material";
-import React from "react";
 
 const Header = () => {
-  const whiteStyle = {
-    backgroundColor: "white",
-  };
-
   return (
     <>
       <AppBar position="fixed">
