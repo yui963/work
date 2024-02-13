b42b3ea0be3e8b5c8a58504e1209e9d6ddb357af
yui963 <myyy20021225@yahoo.ne.jp>
2024-02-13 21:15:17 +0900

    update extension

--- src/vscode-weather/src/extension.ts
+++ src/vscode-weather/src/extension.ts
@@ -30,11 +30,83 @@ interface Area {
   tempsMaxLower: string[];
   reliabilities: string[];
 }
-const AreaList: string[] = ["中部", "東部", "西部", "伊豆"];
-let location: string = "";
-const area_code: number = 220000;
+interface DayObject {
+  day: number;
+  str: string;
+}
+interface AreaObject {
+  name: string;
+  code: string;
+  area: string[];
+}
+const dayList: DayObject[] = [
+  { day: 0, str: "今日" },
+  { day: 1, str: "明日" },
+  { day: 2, str: "明後日" },
+];
 
+const yamagata: string[] = ["村山", "置賜", "庄内", "最上"];
+const shizuoka: string[] = ["中部", "東部", "西部", "伊豆"];
+const PrefectureList: AreaObject[] = [
+  { name: "山形", code: "060000", area: yamagata },
+  { name: "静岡", code: "220000", area: shizuoka },
+];
+const quickPickItems = PrefectureList.map((prefecture) => ({
+  label: prefecture.name,
+}));
+let location: string = "";
+let area_code: string = "";
+let prefecture: string = "";
 export function activate(context: vscode.ExtensionContext) {
+  const change = vscode.commands.registerCommand(
+    "vscode-weather.change",
+    async () => {
+      await vscode.window
+        .showQuickPick(quickPickItems, {
+          placeHolder: "都道府県を選択してください",
+        })
+        .then((place) => {
+          if (place !== undefined) {
+            prefecture = place.label;
+            context.workspaceState.update("prefecture", prefecture);
+            PrefectureList.forEach((prefecture) => {
+              if (prefecture.name === place.label) {
+                area_code = prefecture.code;
+                context.workspaceState.update("area_code", area_code);
+                vscode.window
+                  .showQuickPick(prefecture.area, {
+                    placeHolder: "地域を選択してください",
+                  })
+                  .then((option) => {
+                    if (option !== undefined) {
+                      location = option;
+                      console.log(location);
+                      context.workspaceState.update(
+                        "weatherLocation",
+                        location
+                      );
+                      searchWeather()
+                        .then((result) => {
+                          setJumpCommand(button, result);
+                        })
+                        .catch((error) => console.error(error));
+                    } else {
+                      vscode.window.showInformationMessage("地域未選択");
+                      location = "";
+                      setChangeCommand(button);
+                    }
+                  });
+              }
+            });
+          } else {
+            vscode.window.showInformationMessage("都道府県未選択");
+            area_code = "";
+            setChangeCommand(button);
+          }
+        });
+    }
+  );
+  context.subscriptions.push(change);
   const jump = vscode.commands.registerCommand("vscode-weather.jump", () => {
     vscode.env.openExternal(
       vscode.Uri.parse(
@@ -44,36 +116,19 @@ export function activate(context: vscode.ExtensionContext) {
     );
     context.subscriptions.push(jump);
   });
-  const change = vscode.commands.registerCommand(
-    "vscode-weather.change",
-    async () => {
-      await vscode.window.showQuickPick(AreaList).then((option) => {
-        if (option !== undefined) {
-          location = option;
-        } else {
-          vscode.window.showInformationMessage("未選択");
-        }
-      });
-      console.log(location);
-      context.workspaceState.update("weatherLocation", location);
-      searchWeather()
-        .then((result) => {
-          setJumpCommand(button, result);
-        })
-        .catch((error) => console.error(error));
-    }
-  );
-  context.subscriptions.push(change);
-
   const button = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Right,
     10
   );
-  const savedPosition = context.workspaceState.get("weatherLocation", "");
-  if (!savedPosition) {
+  const savedLocation = context.workspaceState.get("weatherLocation", "");
+  const savedArea_code = context.workspaceState.get("area_code", "");
+  const savedPrefecture = context.workspaceState.get("prefecture", "");
+  if (!savedLocation && !savedArea_code && !savedPrefecture) {
     setChangeCommand(button);
   } else {
-    location = savedPosition;
+    area_code = savedArea_code;
+    location = savedLocation;
+    prefecture = savedPrefecture;
     searchWeather()
       .then((result) => {
         setJumpCommand(button, result);
@@ -99,14 +154,21 @@ function searchWeather(): Promise<string> {
         areas.forEach((part) => {
           if (part.area.name === location) {
             const weatherList: string[] = part.weathers;
-            weatherList.forEach((weather) => {
+            weatherList.forEach((weather, index) => {
               console.log(weather);
               if (weather.includes("雨")) {
-                resolve(location + "：近日雨");
-              } else {
-                resolve(location + "：近日晴れ");
+                resolve(
+                  prefecture +
+                    "（" +
+                    location +
+                    "）" +
+                    "：" +
+                    dayList[index].str +
+                    "雨有り"
+                );
               }
             });
+            resolve(prefecture + "（" + location + "）" + "：晴れ");
           }
         });
       })
