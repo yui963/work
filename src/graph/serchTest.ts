64edc57cedf8f85a2bc959e3b802002e74ace409
yui963 <myyy20021225@yahoo.ne.jp>
2024-07-11 13:29:03 +0900

    create ver0.0

--- src/graph/serchTest.ts
+++ src/graph/serchTest.ts
@@ -1,7 +1,55 @@
 import * as fs from "fs";
-const filePath = "./log-data/data.c";
-function getTestNames(path: string): string[] {
-  const content = fs.readFieSync(path, "utf-8");
-  const testNames: string[] = [];
-  return testNames;
+import * as path from "path";
+
+function searchTestNames(path: string, testNames: string[]): void {
+  let flag: boolean = false;
+  const content = fs.readFileSync(path, "utf-8");
+  const lines = content.split("\n");
+  for (const line of lines) {
+    if (line == "@test") {
+      flag = true;
+    }
+    if (flag) {
+      testNames.push(line);
+      flag = false;
+    }
+  }
 }
+function countTestNum(directoryPath: string): void {
+  const items = fs.readdirSync(directoryPath); //ws-history
+  //item is YYYY-MM-DD
+  for (const item of items) {
+    const testNames: string[] = [];
+    const langPath = path.join(
+      directoryPath,
+      item,
+      "src",
+      "test",
+      "java",
+      "lang"
+    );
+    processDirectory(langPath, testNames);
+    console.log(item + ":" + testNames + ":" + testNames.length);
+  }
+}
+function processDirectory(langPath: string, testNames: string[]): void {
+  processSubdirectory(langPath, testNames, "");
+  processSubdirectory(langPath, testNames, "c");
+  processSubdirectory(langPath, testNames, "c/parse");
+}
+function processSubdirectory(
+  langPath: string,
+  testNames: string[],
+  subDir: string
+): void {
+  const subPath = path.join(langPath, subDir);
+  const subItems = fs.readdirSync(subPath);
+
+  for (const item of subItems) {
+    const fullPath = path.join(subPath, item);
+    searchTestNames(fullPath, testNames);
+  }
+}
+
+const filePath = "./log-data/~/~/~/~";
+countTestNum(filePath);
