410d3f51424e0dd775139f2504893dc726a4598c
yui963 <95596287+yui963@users.noreply.github.com>
2024-12-24 15:28:23 +0900

    sh edit

--- src/code-timechart/run.sh
+++ src/code-timechart/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-ID = $1
-ts-node ./searchTest.ts $ID 7
-if [ $? -eq 0 ]; then
-    ts-node ./createGoogleTimeChart.ts $ID
+json_file="./output/unziped.json"
+size=$(jq '.data | length' "$json_file")
+for((i=0;i<size;i++)); do
+    id=$(jq -r ".data[$i].studentID" "$json_file")
+    session=$(jq ".data[$i].session | map(select(. == 1)) | length" "$json_file")
+    ts-node ./searchTest.ts "$id" "$session"
     if [ $? -eq 0 ]; then
-        ts-node ./savePassRatio.ts $ID 7
+        ts-node ./createGoogleTimeChart.ts "$id"
         if [ $? -eq 0 ]; then
-            ts-node ./editGraphHTML.ts $ID 7
+            ts-node ./savePassRatio.ts "$id" "$session"
+            if [ $? -eq 0 ]; then
+                ts-node ./editGraphHTML.ts "$id" "$session"
+            fi
         fi
     fi
-fi
+done
