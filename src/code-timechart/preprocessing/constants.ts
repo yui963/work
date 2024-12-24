68dc370fdb338dca9100ea6101f2f4b1db566550
yui963 <95596287+yui963@users.noreply.github.com>
2024-12-24 15:59:51 +0900

    preprocessingのパスを書き換えるところから

---
+++ src/code-timechart/preprocessing/constants.ts
@@ -0,0 +1,17 @@
+export namespace KokokonoLabsLogFormatter {
+  export const BASE_DIR: string = "kokokonolabs-log";
+  export const BASE_FORMAT_DIR: string = "kokokonolabs-log-fv01";
+  export const TEST_LOG_DIR: string = "test-results";
+  export const RUN_LOG_DIR: string = "run-results";
+  export const DEBUG_SESSION_DIR: string = "debug-sessions";
+  export const WORKSPACE_HISTORY_DIR: string = "ws-history";
+  export const EDIT_LOG_DIR: string = "edit-activity";
+  export const DATE_TIME_FORMAT: string = "yyyy-MM-dd_HH.mm.ss";
+  export const UNZIPS: string = "Unzips2024";
+  export const FOLDERS: string = "FoldersName";
+  export const VOLUMES: string = "/Volumes";
+  export const DRIVE_ZERO_PATH: string = "/Volumes/exB-0";
+  export const ANALYZE_BASE_PATH: string = "/Volumes/exB-0/unzips";
+  export const UNZIP_STRAGE: string = "D:";
+  export const ZIP_STRAGE: string = "D:";
+}
